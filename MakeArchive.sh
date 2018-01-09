3021911f49be2363913042fa639e422a0622cecc
Sebastien Andrivet <sebastien@advtools.com>
2018-01-09 21:46:27 +0100

    Remove archive from sources

--- MakeArchive.sh
+++ MakeArchive.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-cd HTML; zip -r ../Instances.zip * -x pinegrow.json -x \*_pgbackup/\* -x \*.DS_Store; cd ..
-
+cd HTML; zip -r ../OctoPrint-Instances.zip * -x pinegrow.json -x \*_pgbackup/\* -x \*.DS_Store; cd ..
